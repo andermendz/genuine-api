3f752cd2a65736fc71d25923389da969457484f3
andermendz <andersonmendz19@gmail.com>
2025-02-22 16:28:51 -0500

    test dialog

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\DialogflowController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
+Route::get('/dialogflow', [DialogflowController::class, 'status']);
 Route::post('/dialogflow', [DialogflowController::class, 'handleWebhook']);
