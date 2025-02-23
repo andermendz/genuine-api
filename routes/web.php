24bc1cf76b8102f6a16f9c59cd8e1520ae1e3502
andermendz <andersonmendz19@gmail.com>
2025-02-23 09:08:59 -0500

    /data route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\DialogflowController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/data', function () {
+    return Category::with('products')->get();
 });
